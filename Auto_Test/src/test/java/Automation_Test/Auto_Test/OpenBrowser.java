1fdc77e911dc79cf9984de8d6a7466ea81e7739a
krups210 <krupalgithub210@gmail.com>
2025-07-23 09:26:16 +0530

    Login with error

--- Auto_Test/src/test/java/Automation_Test/Auto_Test/OpenBrowser.java
+++ Auto_Test/src/test/java/Automation_Test/Auto_Test/OpenBrowser.java
@@ -1,14 +1,35 @@
 package Automation_Test.Auto_Test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OpenBrowser {
-	public static void main(String[] args) {
-		System.setProperty("Webdriver.chrome.driver", "A:\\Krupal\\Automation Testing\\Automation Practice\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("Webdriver.chrome.driver","A:\\Krupal\\Automation Testing\\Automation Practice\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.facebook.com/");
+		 driver.get("https://www.facebook.com/");
+		//driver.get("https://brainpayrollwebuat.brainpayroll.co.uk/");
 		driver.manage().window().maximize();
-		driver.close();
+
+		Thread.sleep(3000);
+		
+		WebElement emailElem = driver.findElement(By.id("email"));
+		emailElem.sendKeys("k@gmail.com");
+		WebElement PassElem = driver.findElement(By.id("pass"));
+		PassElem.sendKeys("Admin@123");
+		
+		WebElement loginBtn = driver.findElement(By.name("login"));
+		loginBtn.click();
+		
+		WebElement errmsg = driver.findElement(By.xpath("//*[@id='loginform']/div[2]/div[2]"));
+		if(errmsg.isDisplayed()) {
+			System.out.println("Error message is displayed: " + errmsg.getText());
+		}else {
+			System.out.println("Error message is not displayed:");
+		}
+
+		// driver.close();
 	}
 }
