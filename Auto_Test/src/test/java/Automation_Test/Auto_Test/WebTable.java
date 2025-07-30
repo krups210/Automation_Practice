c172dab02097fe86d78619e46d9033c08e2ebe58
krups210 <krupalgithub210@gmail.com>
2025-07-30 09:31:29 +0530

    webtable

--- Auto_Test/src/test/java/Automation_Test/Auto_Test/WebTable.java
+++ Auto_Test/src/test/java/Automation_Test/Auto_Test/WebTable.java
@@ -2,6 +2,8 @@
 
 package Automation_Test.Auto_Test;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,8 +15,35 @@ public class WebTable {
 
 	//	WebElement Row_name = driver.findElement(By.xpath("//*[@id=\"customers\"]/tbody/tr[4]/td[2]/span"));
 		
-		WebElement Row_name = driver.findElement(By.xpath("//table[@id = 'customers']/tbody/tr[4]/td[2]/span"));
-		System.out.println(Row_name.getText());
+//		WebElement Row_name = driver.findElement(By.xpath("//table[@id = 'customers']/tbody/tr/th[3]"));
+//		System.out.println(Row_name.getText());
+		
+		
+// 		********************************************
+//		List<WebElement> header = driver.findElements(By.xpath("//table[@id = 'customers']/tbody/tr/th"));
+//		for(WebElement e: header) {
+//			System.out.print(e.getText() + " ");
+//		}
+//		System.out.println();
+		
+		
+//		********************************************
+		List<WebElement> header = driver.findElements(By.xpath("//table[@id = 'customers']/tbody/tr/th/span"));
+		//System.out.println(tr.size());
+		for(WebElement e: header) {
+			System.out.print(e.getText() + " ");
+		}
+		System.out.println();
+		
+		List<WebElement> tr = driver.findElements(By.xpath("//table[@id = 'customers']/tbody/tr"));
+		for(int i = 2; i <= tr.size(); i++) {
+			List<WebElement> td = driver.findElements(By.xpath("//table[@id = 'customers']/tbody/tr[" + i + "]/td"));
+			for (int j= 1; j<= td.size(); j++) {
+				WebElement data = driver.findElement(By.xpath("//table[@id = 'customers']/tbody/tr/td[" + j +"]/span"));
+				System.out.print(data.getText() + " ");
+			}
+			System.out.println();
+		}
 		
 	}
 }
